9161eb8111ceed18bf2da2fdccf91db2e4b91391
spaceadvisor <spaceadvisor.com@gmail.com>
2018-03-06 14:56:58 +0800

    上傳招商頁

---
+++ openforout/assets/js/script.js
@@ -0,0 +1,29 @@
+$(function(){
+	inView('._item._t1:not(._sawit) ._mainTitle').on('enter', function(){
+		$('._item._t1').addClass('_sawit');
+	});
+	inView('._item._t2:not(._sawit) ._mainTitle').on('enter', function(){
+		$('._item._t2').addClass('_sawit');
+	});
+	inView('._item._t3:not(._sawit) ._mainTitle').on('enter', function(){
+		$('._item._t3').addClass('_sawit');
+	});
+	inView('._item._t4:not(._sawit) ._mainTitle').on('enter', function(){
+		$('._item._t4').addClass('_sawit');
+	});
+	inView('._item._t5:not(._sawit) ._mainTitle').on('enter', function(){
+		$('._item._t5').addClass('_sawit');
+	});
+	inView('._item._t6:not(._sawit) ._mainTitle').on('enter', function(){
+		$('._item._t6').addClass('_sawit');
+	});
+	inView('._item._t7:not(._sawit) ._mainTitle').on('enter', function(){
+		$('._item._t7').addClass('_sawit');
+	});
+	inView('._item._t8:not(._sawit) ._mainTitle').on('enter', function(){
+		$('._item._t8').addClass('_sawit');
+	});
+	inView('._item._t9:not(._sawit) ._mainTitle').on('enter', function(){
+		$('._item._t9').addClass('_sawit');
+	});
+});
